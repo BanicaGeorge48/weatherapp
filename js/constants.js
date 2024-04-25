1a7c5b5fe2ec6ff37f49c99dcd85cd68266372b0
GEO <146426440+BanicaGeorge48@users.noreply.github.com>
2024-04-25 22:34:13 +0300

    Add files via upload

---
+++ js/constants.js
@@ -0,0 +1,13 @@
+export const API_KEY = 'c134c62de60079ba3a283cf53062ffcb'
+export const BASE_API = 'https://api.openweathermap.org/data/2.5/'
+export const weatherConditionsCodes = {
+    2: 'rainy',
+    3: 'drizzle',
+    5: 'rainy',
+    6: 'snow',
+    7: 'cloudy',
+    8: 'clean',
+
+
+
+}
---
+++ js/geolocation.js
@@ -0,0 +1,39 @@
+
+function geolocationSupport(){
+    // if ('geolocation' in navigator) {
+    //     return true
+    // }
+    // return false
+    return 'geolocation' in navigator
+} 
+
+const defaultOptions ={
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 10000000
+}
+
+export function getCurrentPosition(options = defaultOptions) {
+    if (!geolocationSupport()) throw new Error('You do not have geolocation support')
+
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+            resolve(position)
+            // console.log(lat, lon)
+            // console.log('esto ES  getCurrentPosition')
+        }, () => {
+            reject('no hemos podido obtener tu ubicación')
+        }, options)
+    })
+}
+
+export async function getLatLon(options = defaultOptions) {
+    try {
+        const { coords: { latitude: lat, longitude: lon } } = await getCurrentPosition(options)
+        return { lat, lon, isError: false }
+      } catch {
+        return { isError: true, lat: null, lon: null }
+    }
+}
